5f1d0b5d61b2c8990165e80b0d3540cc86503b69
Felipe Freitas dos Santos Silva <felipeprofissional22@gmail.com>
2025-04-01 15:41:59 -0300

    remove unused SVG files and update global styles; add Header and Hero components

---
+++ src/components/header/index.tsx
@@ -0,0 +1,52 @@
+import icons from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center  m-13">
+      <div className="text-2xl font-bold">Felipe Dev</div>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>
+            <a href="/" className="hover:text-gray-400 px-10">
+              Incio
+            </a>
+          </li>
+
+          <li>
+            <a href="/" className="hover:text-gray-400 px-10">
+              Projetos
+            </a>
+          </li>
+          <li>
+            <a href="/" className="hover:text-gray-400 px-10">
+              Contato
+            </a>
+          </li>
+
+          <li>
+            <a href="/" className="hover:text-gray-400 px-10">
+              Serviços
+            </a>
+          </li>
+
+          <li>
+            <a className="text-gray-400 hover:text-gray-800 transition-colors duration-300 ease-out" href="https://www.linkedin.com/in/felipesansi" target="_blank" rel="noopener noreferrer">
+              <FaLinkedin className="text-3xl" />
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-gray-400 hover:text-gray-800 transition-colors duration-300 ease-in-out"
+              href="https://github.com/felipesansi"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub className="text-3xl" />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
