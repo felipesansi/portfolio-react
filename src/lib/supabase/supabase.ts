99e7a89bf24f9fff7eadda0cbce2e71815d6f039
Felipe Freitas dos Santos Silva <felipeprofissional22@gmail.com>
2025-09-05 11:21:14 -0300

    Alterando Lib

--- src/lib/supabase/supabase.ts
+++ src/lib/supabase/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import { SUPABASE_ANON_KEY, SUPABASE_URL } from '../../../constants/supabase';
 
 // URL e chave do Supabase (vêm do seu constants/supabase)
-const supabaseUrl = SUPABASE_URL;
-const supabaseKey = SUPABASE_ANON_KEY;
+const supabaseUrl =process.env.NEXT_PUBLIC_SUPABASE_URL || SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || SUPABASE_ANON_KEY;
 
 // Cliente pronto para ser usado em todo o app
 export const supabase = createClient(supabaseUrl, supabaseKey);
